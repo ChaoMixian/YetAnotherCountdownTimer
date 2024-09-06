ea212f5d020fd9a88e974a5e8e3ee5813079ba2d
ChaoMixian <chaomingxun@outlook.com>
2024-09-06 18:09:33 +0800

    fix path error

--- src/main.js
+++ src/main.js
@@ -5,59 +5,6 @@ const path = require('path');
 const mainWindow = null
 let tray = null
 
-// 创建托盘
-function createTray() {
-  // 系统托盘： window 系统
-  if (process.platform === 'win32') {
-    const icon =
-      process.env.NODE_ENV === 'development'
-        ? path.join(__dirname, '../assets/icon.ico')
-        : path.join(__dirname, '../assets/icon.ico') // 指定托盘图标，推荐使用 ico 图标。
-
-    // 创建托盘实例
-    tray = new Tray(icon)
-    // 上面托盘实例创建了，需要定义托盘中的 Menu 选项。
-    // 这里使用 buildFromTemplate 静态方法。
-    // 第一个：开机自启动
-    // 第二个：退出
-    let menu = Menu.buildFromTemplate([
-      {
-        label: '开机启动',
-        checked: app.getLoginItemSettings().openAtLogin, // 获取当前自启动状态
-        type: 'checkbox',
-        click: () => {
-          // 点击事件：切换自启动
-          if (!app.isPackaged) { // 生成环境
-            app.setLoginItemSettings({
-              openAtLogin: !app.getLoginItemSettings().openAtLogin,
-              path: process.execPath
-            })
-          } else {
-            app.setLoginItemSettings({
-              openAtLogin: !app.getLoginItemSettings().openAtLogin
-            })
-          }
-        }
-      },
-      {
-        label: '退出',
-        click: function () {
-          app.quit()
-          app.quit()
-        }
-      }
-    ])
-    // 鼠标悬停时显示的文本
-    tray.setToolTip('YACT')
-    // 设置上下文菜单
-    tray.setContextMenu(menu)
-    // 绑定点击事件：控制 窗口显示和隐藏。
-    tray.on('click', () => {
-      mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
-    })
-  }
-}
-
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 300,
@@ -93,10 +40,12 @@ function createWindow() {
 
   // 系统托盘： window 系统
   if (process.platform === 'win32') {
-    const icon =
-      process.env.NODE_ENV === 'development'
-        ? path.join(__dirname, '../assets/icon.ico')
-        : path.join(__dirname, '../assets/icon.ico') // 指定托盘图标，推荐使用 ico 图标。
+    const icon = path.join(__dirname, '../assets/icon.ico')
+    // if (!app.isPackaged) {
+    //   const icon = path.join(__dirname, '../assets/icon.ico') // 指定托盘图标
+    // }else {
+    //   const icon = path.join(__dirname, 'icon.ico') // 指定托盘图标
+    // }
 
     // 创建托盘实例
     tray = new Tray(icon)
